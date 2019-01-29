0480d6a5d7a855532d0ae57f7e4982cda319d411
Alejandro Borria <aborria@summasolutions.net>
2019-01-29 10:51:53 -0300

    append New cron tab and duplicated run validation

--- app/code/Limesharp/StockImport/Cron/Import.php
+++ app/code/Limesharp/StockImport/Cron/Import.php
@@ -1,12 +1,16 @@
 <?php
 namespace Limesharp\StockImport\Cron;
 
+use \Magento\Cron\Model\Schedule;
+
 class Import {
 
     /**
      * CRON CONFIG FOR STOCK PRODUCT IMPORT
      */
     const CRON_ACTIVE_CONFIG = 'limesharp/stockimport/cron_active';
+
+    const CRON_JOB_CODE = 'limesharp_stockimport';
 
     /**
      * Logger
@@ -29,20 +33,27 @@ class Import {
      */
     protected $scopeConfig;
 
+    /**
+     * @var \Magento\Cron\Model\ScheduleFactory
+     */
+    protected $_scheduleFactory;
 
     /**
      * @param \Psr\Log\LoggerInterface $logger
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeInterface
      * @param \Limesharp\StockImport\Helper\Import $helperImport
+     * @param \Magento\Cron\Model\ScheduleFactory $scheduleFactory
      */
     public function __construct(
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeInterface,
-        \Limesharp\StockImport\Helper\Import $helperImport
+        \Limesharp\StockImport\Helper\Import $helperImport,
+        \Magento\Cron\Model\ScheduleFactory $scheduleFactory
     ) {
         $this->logger = $logger;
         $this->scopeConfig = $scopeInterface;
         $this->importer = $helperImport;
+        $this->scheduleFactory = $scheduleFactory;
     }
 
     public function execute() {
@@ -52,15 +63,33 @@ class Import {
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
 
-        if ($isActive == 1) {
+        // Check if cron is active and if there is more than 1 cron running
+        if ($isActive == 1 && $this->getCronImportsRunning()->getSize() <= 1) {
             $this->logger->info('Limesharp_StockImport: Cron Import Started');
 
             // Instance Logic into Helper to can use it without the Cron
             $this->importer->execute();
 
             $this->logger->info('Limesharp_StockImport: Cron Import Finished');
+        } else {
+            if ($this->getCronImportsRunning()->getSize()){
+                $this->logger->info('Limesharp_StockImport: Another Stock Import cron is running');
+            }
         }
 
         return $this;
+    }
+
+    /**
+     * Return job collection from data base with status 'running'
+     *
+     * @return $pendingImportJobs \Magento\Cron\Model\ResourceModel\Schedule\Collection
+     */
+    private function getCronImportsRunning()
+    {
+        $pendingImportJobs = $this->scheduleFactory->create()->getCollection();
+        $pendingImportJobs->addFieldToFilter('job_code', self::CRON_JOB_CODE);
+        $pendingImportJobs->addFieldToFilter('status', Schedule::STATUS_RUNNING);
+        return $pendingImportJobs;
     }
 }
